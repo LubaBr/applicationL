510980b3aed6295683a50016cc2942d3d0b4841c
Luba Bridgelal <luba.stylmashenko@gmail.com>
2019-07-09 14:01:02 -0400

    url encode and base64 the answer

--- brokerLift/index.php
+++ brokerLift/index.php
@@ -2,10 +2,13 @@
 
 require_once('settings.php');
 
+//echo base64_encode(urlencode('NOW!'));exit;
+//TEST value '?answer=Tk9XJTIx';
+
 //check _REQUEST for view
 $setting = new Settings();
 $auth_item = new AuthenticationLight($setting);
-$answer = filter_input(INPUT_GET,'answer',FILTER_SANITIZE_STRING);
+$answer = urldecode(base64_decode(filter_input(INPUT_GET,'answer',FILTER_SANITIZE_STRING)));
 $view = $auth_item->checkEntry($answer);
 
 /*echo '<pre>';
@@ -62,7 +65,6 @@ echo '</pre>';
   </div>
 
   <hr>
-
 </div> <!-- /container -->
 
 </main>
@@ -72,7 +74,10 @@ echo '</pre>';
     <p>&copy; Lsoftware 2019 @LubaBridgelal</p>
   </footer>
 
-  <script src="../js/script.js"></script>
+  <script>
+
+
+  </script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
